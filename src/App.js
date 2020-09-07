85b88469a3eb7e70b48f65e1a033b81115e34889
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-07 09:15:33 +0200

    declared useEffect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 const App = () => {
+  useEffect(() => {}, []);
+
   return (
     <div>
       <h1>Alan AI News Application</h1>
