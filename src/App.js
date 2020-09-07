2f4e2cbb140783ace875973416aa4a44c4b6f10b
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-07 09:15:58 +0200

    call alanBtn inside useEffect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 const App = () => {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    alanBtn({
+      
+    })
+  }, []);
 
   return (
     <div>
