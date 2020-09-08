ee3b18b681996ee17b750baa1ddd697ece846d98
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-08 09:12:38 +0200

    passed i

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -8,7 +8,7 @@ const NewsCards = ({ articles }) => {
       <Grid container alignItems='stretch' spacing={3}>
         {articles.map((article, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
-            <NewsCard key={i} article={article} />
+            <NewsCard i={i} article={article} />
           </Grid>
         ))}
       </Grid>
