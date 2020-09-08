28c851807d91b943d4adc5a1e44866732867893d
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-08 09:15:01 +0200

    added classname to the Grid container

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -5,7 +5,12 @@ import NewsCard from '../NewsCard/NewsCard';
 const NewsCards = ({ articles }) => {
   return (
     <Grow in>
-      <Grid container alignItems='stretch' spacing={3}>
+      <Grid
+        classname={classes.container}
+        container
+        alignItems='stretch'
+        spacing={3}
+      >
         {articles.map((article, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
             <NewsCard i={i} article={article} />
