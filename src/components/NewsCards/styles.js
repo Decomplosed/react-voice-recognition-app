33cc8704dc96a9b0fc0b4e252299f1823794a4c4
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-08 09:17:34 +0200

    added styling for container

--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -1,7 +1,11 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const styles = makeStyles({
-  container: {},
+  container: {
+    padding: '0 5%',
+    width: '100%',
+    margin: 0,
+  },
 });
 
 export default styles;
