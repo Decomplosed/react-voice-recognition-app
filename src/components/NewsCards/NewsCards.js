a6d5848cfa42cbb53a78a50307522dfcc9e6bb77
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-08 09:09:20 +0200

    added spacing value to Grid

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -5,7 +5,7 @@ import NewsCard from '../NewsCard/NewsCard';
 const NewsCards = ({ articles }) => {
   return (
     <Grid in>
-      <Grid container alignItems='stretch'>
+      <Grid container alignItems='stretch' spacing={3}>
         {articles.map((article, i) => (
           <NewsCard key={i} />
         ))}
