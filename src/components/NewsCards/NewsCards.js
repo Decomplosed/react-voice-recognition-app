d5c6cd17fca9c3e617a1debc2d4776fe50540b97
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-10 23:32:27 +0200

    added key to the Grdi container

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -16,7 +16,15 @@ const NewsCards = ({ articles }) => {
         spacing={3}
       >
         {articles.map((article, i) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+          <Grid
+            key={i}
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            lg={3}
+            style={{ display: 'flex' }}
+          >
             <NewsCard i={i} article={article} />
           </Grid>
         ))}
