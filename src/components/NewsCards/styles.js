eb67d04e4b5824c81e4af02af05546a69bc13b0c
Bart Frackiewicz <decomplosed@gmail.com>
2020-09-10 23:48:19 +0200

    styled card

--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -6,6 +6,17 @@ const styles = makeStyles({
     width: '100%',
     margin: 0,
   },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '45vh',
+    padding: '10%',
+    borderRadius: 10,
+    color: 'white',
+  },
 });
 
 export default styles;
